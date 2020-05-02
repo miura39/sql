9b8e5fae83a317eef104fcf68105b222c286fee8
miura39 <miurast.miura.y@gmail.com>
2020-05-03 02:26:59 +0900

    SQL構文完成　データベース作成

--- app/views/signups/users.sql
+++ app/views/signups/users.sql
@@ -6,40 +6,43 @@ DROP DATABASE IF EXISTS shop;
 CREATE DATABASE shop;
 
 /*DB選択*/
+USE shop;
+
+/*商品テーブル作成*/
 DROP TABLE IF EXISTS products;
-CREATE TABLE products;
+CREATE TABLE products
 (
-      product_code    CHAR(5)PRIMARY KEY,
-      product_name    VARCHAR(50) NOT NULL
-      unit_price      INTEGER NOT NULL
-      categories      CHAR(1) NOT NULL
+      product_code    CHAR(5) PRIMARY KEY,
+      product_name    VARCHAR(50) NOT NULL,
+      unit_price      INTEGER NOT NULL,
+      categories      CHAR(1) NOT NULL,
       related_product CHAR(5)
-)ENGINE = INNODB;
+)ENGINE = InnoDB;
 
 /*注文テーブル作成*/
-DROP TABLE IF EXISTS order;
-CREATE TABLE order
+DROP TABLE IF EXISTS lf_order;
+CREATE TABLE lf_order
 (
       order_date          DATE,
       order_no            CHAR(12),
       order_branch_no     INTEGER,
-      product_code        CHAR(5)NOT NULL,
+      product_code        CHAR(5) NOT NULL,
       quantity            INTEGER,
       coupon_discount_fee INTEGER
-)ENGINE = INNODB;
-ALTER TABLE order ADD PRIMARY KEY(order_date,order_no,order_branch_no);
+)ENGINE = InnoDB;
+ALTER TABLE lf_order ADD PRIMARY KEY(order_date,order_no,order_branch_no);
 
 /*廃盤商品テーブル作成*/
 DROP TABLE IF EXISTS db_products;
-CREATE TABLE db_products;
+CREATE TABLE db_products
 (
-      product_code                CHAR(5)PRIMARY KEY,
-      product_name                VARCHAR(50) NOT NULL
-      unit_price                  INTEGER NOT NULL
-      categories                  CHAR(1) NOT NULL
+      product_code                CHAR(5) PRIMARY KEY,
+      product_name                VARCHAR(50) NOT NULL,
+      unit_price                  INTEGER NOT NULL,
+      categories                  CHAR(1) NOT NULL,
       discountinued_product_date  DATE,
       seles_quantity              INTEGER NOT NULL
-)ENGINE = INNODB;
+)ENGINE = InnoDB;
 
 INSERT INTO products VALUES('A0001','秋のニットスカート',3800,1,NULL);
 INSERT INTO products VALUES('A0052','裏起毛パーカー',2900,1,NULL);
@@ -61,7 +64,7 @@ INSERT INTO products VALUES('F0191','イタリアンレザー長財布',15000,3,
 INSERT INTO products VALUES('F0194','アンティーク調コインケース',8800,3,NULL);
 INSERT INTO products VALUES('M0085','本革ビジネスバッグ',13000,3,NULL);
 INSERT INTO products VALUES('M0122','ニットポシェット',3300,3,NULL);
-INSERT INTO products VALUES('M0085','3WAYななめがけバッグ',5000,3,NULL);
+INSERT INTO products VALUES('M0310','3WAYななめがけバッグ',5000,3,NULL);
 INSERT INTO products VALUES('N0119','マグカップ型スピーカー',1350,9,NULL);
 INSERT INTO products VALUES('N8810','赤ちゃん用肌着',980,9,NULL);
 INSERT INTO products VALUES('N8990','マグネット（花）',350,9,NULL);
@@ -71,7 +74,7 @@ INSERT INTO products VALUES('N9999','テストデータ',99999,9,NULL);
 INSERT INTO products VALUES('S0017','形状記憶ワイシャツ',2900,1,NULL);
 INSERT INTO products VALUES('S0023','チュールのコサージュ',550,3,NULL);
 INSERT INTO products VALUES('S0604','長め丈Tシャツ',1500,1,NULL);
-INSERT INTO products VALUES('S0740','綿のジャケット',3800,1,NULL);
+INSERT INTO products VALUES('S1003','綿のジャケット',3800,1,NULL);
 INSERT INTO products VALUES('S1056','サマーロングスカート',2800,1,NULL);
 INSERT INTO products VALUES('S0740','水玉のワンピース',2600,1,NULL);
 INSERT INTO products VALUES('W0012','ニットセーター',3500,1,NULL);
@@ -93,14 +96,20 @@ COMMIT;
 -- LINE TERMINATED BY "¥r¥n";
 -- COMMIT;
 
-/* csvファイルからデータのロード*/
-LOAD DATA INFILE "ファイル場所指定：order.csv"
-INTO TABLE order FIELDS TERMINATED BY ","
-LINE TERMINATED BY "¥r¥n";
+/* csvファイルからデータのロード
+*CSVデータはtmpに入れないと読み込まない
+*SELECT @@global.secure_file_priv;を空にする必要がある。
+*/
+LOAD DATA LOCAL INFILE "/tmp/order.csv"
+INTO TABLE lf_order 
+FIELDS TERMINATED BY ',';
 COMMIT;
 
-/* csvファイルからデータのロード*/
-LOAD DATA INFILE "ファイル場所指定：products.csv"
-INTO TABLE db_products FIELDS TERMINATED BY ","
-LINE TERMINATED BY "¥r¥n";
+/* csvファイルからデータのロード
+*CSVデータはtmpに入れないと読み込まない
+*SELECT @@global.secure_file_priv;を空にする必要がある。
+*/
+LOAD DATA LOCAL INFILE "/tmp/discontinued_products.csv"
+INTO TABLE db_products 
+FIELDS TERMINATED BY ',';
 COMMIT;
