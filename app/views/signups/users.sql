9f522160c185fcf6d714b46ea07f69df994b389d
miura39 <miurast.miura.y@gmail.com>
2020-05-02 20:29:31 +0900

    sql構文　ショッピングsqlデータの作成

--- app/views/signups/users.sql
+++ app/views/signups/users.sql
@@ -1,1 +1,17 @@
-select * from users
+/*AUTOCOMMIT1無効*/
+SET AUTOCOMMIT=0;
+
+/*商品データベース作成*/
+DROP DATABASE IF EXISTS shop;
+CREATE DATABASE shop;
+
+/*DB選択*/
+DROP TABLE IF EXISTS products;
+CREATE TABLE products;
+(
+      product_code    CHAR(5)PRIMARY KEY,
+      product_name    VARCHAR(50) NOT NULL
+      unit_price      INTEGER NOT NULL
+      categories      CHAR(1) NOT NULL
+      related_product CHAR(5)
+)
