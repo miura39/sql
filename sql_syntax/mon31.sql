5462660b1ce7721e7d85193b4eacfefda5497e7f
miura39 <miurast.miura.y@gmail.com>
2020-05-04 16:38:17 +0900

    mon31 FIX 商品テーブルから、これまでに注文されたことのない商品コードを商品コードの昇順に抽出する

--- sql_syntax/mon31.sql
+++ sql_syntax/mon31.sql
@@ -1,8 +1,7 @@
 /*商品テーブルから、これまでに注文されたことのない商品コードを
 商品コードの昇順に抽出する*/
-SELECT  product_code
+SELECT  *
 FROM    products
-UNION
-SELECT  product_code
-FROM    lf_order
-ORDER BY product_code ASC
+LEFT OUTER JOIN  lf_order
+ON products.product_code IS NULL = lf_order.product_code IS NULL
+ORDER BY products.product_code
