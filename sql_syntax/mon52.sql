da7701d616afb66ab695ffb88eebc4bb6eac7f0d
miura39 <miurast.miura.y@gmail.com>
2020-05-05 19:08:56 +0900

    mon53 FIX　COUNTに OR NULLを入力

--- sql_syntax/mon52.sql
+++ sql_syntax/mon52.sql
@@ -1,9 +1,7 @@
 /*これまでにクーポン割引した注文件数と、割引額の合計を求める。
 ただしWHERE句による条件を指定しないこと。*/
 SELECT
-  COUNT(coupon_discount_fee) AS 'クーポン回数',
+  COUNT(coupon_discount_fee > 0 OR NULL) AS 'クーポン回数',
   SUM(coupon_discount_fee ) AS '割引額合計'
 FROM    
   lf_order
-GROUP BY coupon_discount_fee <> 0
-ORDER BY coupon_discount_fee DESC
